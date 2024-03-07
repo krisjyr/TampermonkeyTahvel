975f3842bed609c290ed7bf1f98e8a74b896dfd5
krisjyr <113598502+krisjyr@users.noreply.github.com>
2024-03-07 14:01:53 +0200

    I want to kill myself

--- login.user.js
+++ login.user.js
@@ -9,24 +9,55 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    var loginbtn = document.getElementById("login-button-hitarea")
-    var content = document.getElementById("content-and-sidenav-wrapper")
+(function () {
+  "use strict";
 
-    document.getElementById("site-sidenav-wrapper").remove()
-    document.getElementById("main-content").remove()
-    document.getElementById("help-menu").remove()
-    document.getElementById("main-menu-button-wrapper").remove()
+  function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+      begin = dc.indexOf(prefix);
+      if (begin != 0) return null;
+    } else {
+      begin += 2;
+      var end = document.cookie.indexOf(";", begin);
+      if (end == -1) {
+        end = dc.length;
+      }
+    }
+    // because unescape has been deprecated, replaced with decodeURI
+    //return unescape(dc.substring(begin + prefix.length, end));
+    return decodeURI(dc.substring(begin + prefix.length, end));
+  }
 
-    loginbtn.click()
-    loginbtn.remove()
+  function killme() {
+    var myCookie = getCookie("XSRF-TOKEN");
 
-    document.querySelector(".md-dialog-backdrop").remove()
-    content.appendChild(document.querySelector(".md-dialog-size"))
-    var style = document.createElement('style')
+    if (myCookie != null) {
+      // do cookie doesn't exist stuff;
 
-    style.innerHTML = `md-dialog .md-tab-markup {
+      console.log("Exist");
+    } else {
+      console.log("Doesn' exist");
+
+      var loginbtn = document.getElementById("login-button-hitarea");
+      var content = document.getElementById("content-and-sidenav-wrapper");
+
+      document.getElementById("site-sidenav-wrapper").remove();
+      document.getElementById("main-content").remove();
+      document.getElementById("help-menu").remove();
+      document.getElementById("main-menu-button-wrapper").remove();
+      document.getElementById("footer").remove();
+
+      loginbtn.click();
+      loginbtn.remove();
+
+      document.querySelector(".md-dialog-backdrop").remove();
+      content.appendChild(document.querySelector(".md-dialog-size"));
+      var style = document.createElement("style");
+
+      style.innerHTML = `md-dialog .md-tab-markup {
     width: 100%;
   }
   md-dialog .md-button {
@@ -38,22 +69,31 @@
     background: rgb(255,64,129);
   }
   .md-dialog-size {
-    width: 400px;
-    height: 400px;
+    width: 50%;
+    height: 100%;
   }
   @media (max-width: 600px) {
   .md-dialog-size {
     height: 450px;
   }
-}`
-    
-    content.style
+}`;
 
-    content.appendChild(style)
+      content.style.cssText +=
+        "display:flex;justify-content:center;margin-inline:auto;width:100%;height:80%;margin-block:14rem";
+      document.querySelector(".md-padding").style.cssText +=
+        "height:max-content;width:max-content";
+      document.querySelector(".md-button").style.cssText =
+        "height:100%;width:100%";
 
-    document.querySelector(".md-dialog-container").remove()
-    document.querySelector(".md-scroll-mask").remove()
-    document.querySelector(".md-toolbar-tools").remove()
+      content.appendChild(style);
 
+      document.querySelector(".md-dialog-container").remove();
+      document.querySelector(".md-scroll-mask").remove();
+      document.querySelector(".md-toolbar-tools").remove();
+    }
+  }
 
+  setTimeout(function () {
+    killme();
+  }, 2000);
 })();
