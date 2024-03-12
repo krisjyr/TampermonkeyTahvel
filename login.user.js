ade14c112907f0c3303226c7dd9c62c5cdb95eaa
Mqxsyy <113341234+Mqxsyy@users.noreply.github.com>
2024-03-12 14:01:54 +0200

    Tegime pat

--- login.user.js
+++ login.user.js
@@ -12,88 +12,64 @@
 (function () {
   "use strict";
 
-  function getCookie(name) {
-    var dc = document.cookie;
-    var prefix = name + "=";
-    var begin = dc.indexOf("; " + prefix);
-    if (begin == -1) {
-      begin = dc.indexOf(prefix);
-      if (begin != 0) return null;
-    } else {
-      begin += 2;
-      var end = document.cookie.indexOf(";", begin);
-      if (end == -1) {
-        end = dc.length;
-      }
+    function getCookie(name) {
+        const dc = document.cookie;
+        const prefix = name + "=";
+        let begin = dc.indexOf("; " + prefix);
+        let end;
+
+        if (begin == -1) {
+            begin = dc.indexOf(prefix);
+            if (begin != 0) return null;
+        } else {
+            begin += 2;
+            end = document.cookie.indexOf(";", begin);
+
+            if (end == -1) {
+                end = dc.length;
+            }
+        }
+
+        return decodeURI(dc.substring(begin + prefix.length, end));
     }
-    // because unescape has been deprecated, replaced with decodeURI
-    //return unescape(dc.substring(begin + prefix.length, end));
-    return decodeURI(dc.substring(begin + prefix.length, end));
-  }
 
-  function killme() {
-    var myCookie = getCookie("XSRF-TOKEN");
+    function Login() {
+        var myCookie = getCookie("XSRF-TOKEN");
 
-    if (myCookie != null) {
-      // do cookie doesn't exist stuff;
+        if (myCookie != null) {
+            console.log("Logged In")
+            MainPage();
+            return
+        }
 
-      console.log("Exist");
-    } else {
-      console.log("Doesn' exist");
-
-      var loginbtn = document.getElementById("login-button-hitarea");
-      var content = document.getElementById("content-and-sidenav-wrapper");
-
-      document.getElementById("site-sidenav-wrapper").remove();
-      document.getElementById("main-content").remove();
-      document.getElementById("help-menu").remove();
-      document.getElementById("main-menu-button-wrapper").remove();
-      document.getElementById("footer").remove();
-
-      loginbtn.click();
-      loginbtn.remove();
-
-      document.querySelector(".md-dialog-backdrop").remove();
-      content.appendChild(document.querySelector(".md-dialog-size"));
-      var style = document.createElement("style");
-
-      style.innerHTML = `md-dialog .md-tab-markup {
-    width: 100%;
-  }
-  md-dialog .md-button {
-    padding: 0 0px;
-    margin: 0px 0px 0px 0px;
-  }
-  md-dialog .line {
-    width : 100%;
-    background: rgb(255,64,129);
-  }
-  .md-dialog-size {
-    width: 50%;
-    height: 100%;
-  }
-  @media (max-width: 600px) {
-  .md-dialog-size {
-    height: 450px;
-  }
-}`;
-
-      content.style.cssText +=
-        "display:flex;justify-content:center;margin-inline:auto;width:100%;height:80%;margin-block:14rem";
-      document.querySelector(".md-padding").style.cssText +=
-        "height:max-content;width:max-content";
-      document.querySelector(".md-button").style.cssText =
-        "height:100%;width:100%";
-
-      content.appendChild(style);
-
-      document.querySelector(".md-dialog-container").remove();
-      document.querySelector(".md-scroll-mask").remove();
-      document.querySelector(".md-toolbar-tools").remove();
+        console.log("Not Logged In")
+        document.querySelector('body').remove();
+        window.location.href = "https://tahvel.edu.ee/hois_back/haridLogin"
+        return
     }
-  }
 
-  setTimeout(function () {
-    killme();
-  }, 2000);
-})();
+    function MainPage() {
+        const sideNav = document.getElementById("sidenav-list");
+
+        sideNav.children[10].remove();
+        sideNav.children[9].remove();
+        sideNav.children[8].remove();
+        sideNav.children[7].remove();
+        sideNav.children[6].remove();
+        sideNav.children[5].remove();
+        sideNav.children[4].remove();
+
+        document.getElementById("repr-link-wrapper").remove();
+        document.getElementById("user-menu-name").innerHTML += "🔫"
+
+        const main = document.getElementById("main-wrapper")
+        main.children[0].children[2].children[3].remove()
+        main.children[0].children[2].children[2].remove()
+
+        document.getElementById("home-data-sections-container-small").children[3].remove()
+        console.log(document.getElementById("home-data-sections-container").children[0].children[1].remove())
+    }
+
+    //Login();
+    setTimeout(Login, 200);
+})()
